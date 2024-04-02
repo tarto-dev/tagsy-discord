4b4dad36ee2d8c7b725af98304201c4a0c1325a8
Clara Cassinat <clara.cassinat@gmail.com>
2024-04-03 00:01:06 +0200

    Fix linters

--- commands/tag_command.py
+++ commands/tag_command.py
@@ -1,6 +1,18 @@
-import disnake
-import re
+"""A module that contains the TagCommands class,
+which is a Cog for handling commands related to tagging messages within Discord servers.
+
+This module provides functionality to add, retrieve, update, delete,
+and reset tagged messages in a database.
+
+Classes:
+- TagCommands:
+A Cog for handling commands related to tagging messages within Discord servers.
+"""
+
 import datetime
+import re
+
+import disnake
 from disnake.ext import commands
 
 # Import functions from the database module to interact with tagged messages.
@@ -15,11 +27,22 @@ from db import (
     update_message,
 )
 
+
 class TagCommands(commands.Cog):
-    """A Cog for handling commands related to tagging messages within Discord servers."""
+    """
+    A Cog for handling commands related to tagging messages within Discord servers.
+    """
 
     def __init__(self, bot):
-        """Initializes the commands cog with a reference to the bot instance."""
+        """
+        Initializes the TagCommand class.
+
+        Args:
+            bot (Bot): The bot instance.
+
+        Returns:
+            None
+        """
         self.bot = bot
 
     @commands.slash_command(name="add", description="Adds a tagged message.")
@@ -28,6 +51,12 @@ class TagCommands(commands.Cog):
     ):
         """
         Adds a tagged message to the database.
+
+        Parameters:
+        - inter: The interaction object representing the slash command interaction.
+        - tag: The tag to be added.
+        - message: The message to be associated with the tag.
+
         If the tag already exists, suggests alternative tags.
         """
         server_id = str(inter.guild.id)
@@ -38,7 +67,8 @@ class TagCommands(commands.Cog):
             recommendations = generate_recommendations(tag)
             recommendations_str = ", ".join(recommendations)
             await inter.response.send_message(
-                f"This tag already exists. Suggestions: {recommendations_str}. The message you wanted to add is ```{message}```",
+                f"This tag already exists. Suggestions: {recommendations_str}."
+                + f"The message you wanted to add is ```{message}```",
                 ephemeral=True,
             )
         else:
@@ -51,6 +81,17 @@ class TagCommands(commands.Cog):
         """
         Retrieves and displays a tagged message from the database.
         If not found, suggests similar tags.
+
+        Parameters:
+        - inter: The interaction object representing the slash command interaction.
+        - tag: The tag to retrieve.
+
+        Returns:
+        - None
+
+        Raises:
+        - disnake.NotFound: If the user who created the tagged message is not found.
+
         """
         server_id = str(inter.guild.id)
         tag_info = await get_message(server_id, tag)
@@ -83,9 +124,18 @@ class TagCommands(commands.Cog):
     )
     async def getall(self, inter: disnake.ApplicationCommandInteraction):
         """
-        Retrieves and displays all tags and their associated messages from the database for the server,
-        including details such as who posted it, when it was posted, and the number of uses.
+        Retrieves and displays all tags and their associated messages
+        from the database for the server, including details such as
+        who posted it, when it was posted, and the number of uses.
+
         Each tagged message is displayed in its own embed.
+
+        Parameters:
+        - inter (disnake.ApplicationCommandInteraction):
+        The interaction object representing the slash command interaction.
+
+        Returns:
+        None
         """
         server_id = str(inter.guild.id)
         tags_details = await get_all_messages(
@@ -116,7 +166,19 @@ class TagCommands(commands.Cog):
     @commands.slash_command(name="remove", description="Deletes a tagged message.")
     async def remove(self, inter: disnake.ApplicationCommandInteraction, tag: str):
         """
-        Deletes a tagged message from the database if the user has the required permissions.
+        Deletes a tagged message from the database if the user
+        has the required permissions.
+
+        Parameters:
+        - inter: The disnake.ApplicationCommandInteraction object
+        representing the interaction with the slash command.
+        - tag: The tag to be deleted.
+
+        Returns:
+        - None
+
+        Raises:
+        - None
         """
         server_id = str(inter.guild.id)
 
@@ -142,7 +204,6 @@ class TagCommands(commands.Cog):
                     f'No message found for tag "{tag}".', ephemeral=True
                 )
 
-    # Add update command
     @commands.slash_command(
         name="update", description="Updates the content of a tagged message."
     )
@@ -151,6 +212,17 @@ class TagCommands(commands.Cog):
     ):
         """
         Updates the content of a tagged message in the database.
+
+        Parameters:
+        - inter: The interaction object representing the slash command interaction.
+        - tag: The tag name to update.
+        - message: The new content of the tagged message.
+
+        Returns:
+        None
+
+        Raises:
+        None
         """
         server_id = str(inter.guild.id)
 
@@ -176,6 +248,16 @@ class TagCommands(commands.Cog):
     async def reset(self, inter: disnake.ApplicationCommandInteraction, tag: str):
         """
         Resets the usage count of a tagged message in the database.
+
+        Parameters:
+        - inter: The interaction object representing the slash command interaction.
+        - tag: The name of the tag to reset the call counter for.
+
+        Returns:
+        - None
+
+        Raises:
+        - None
         """
         server_id = str(inter.guild.id)
 
@@ -192,7 +274,17 @@ class TagCommands(commands.Cog):
     @commands.Cog.listener(name="on_message")
     async def on_message(self, message: disnake.Message):
         """
-        Listens for messages starting with '%' and attempts to retrieve and display the corresponding tagged message.
+        Listens for messages starting with '%' and attempts to retrieve
+        and display the corresponding tagged message.
+
+        Parameters:
+        - message (disnake.Message): The message object that triggered the event.
+
+        Returns:
+        - None
+
+        Raises:
+        - None
         """
         if message.author == self.bot.user or not message.content:
             return
@@ -216,7 +308,8 @@ class TagCommands(commands.Cog):
                 if echo:
                     suggestions = [str(e[0]) for e in echo]
                     await message.channel.send(
-                        f"No message found for tag \"{tag}\". Suggestions: {', '.join(suggestions)}",
+                        f'No message found for tag "{tag}".'
+                        + f"Suggestions: {', '.join(suggestions)}",
                         ephemeral=True,
                     )
                 else:
@@ -226,37 +319,87 @@ class TagCommands(commands.Cog):
 
 
 async def tag_exists(server_id, tag):
-    """Checks if a tag already exists in the database."""
+    """Checks if a tag already exists in the database.
+
+    Args:
+        server_id (int): The ID of the server where the tag is being checked.
+        tag (str): The name of the tag being checked.
+
+    Returns:
+        bool: True if the tag exists in the database, False otherwise.
+    """
     tag_info = await get_message(server_id, tag)
     return tag_info is not None
 
 
 def generate_recommendations(tag):
-    """Generates tag recommendations based on the original tag."""
+    """
+    Generates tag recommendations based on the original tag.
+
+    Parameters:
+    - tag (str): The original tag to generate recommendations for.
+
+    Returns:
+    - list: A list of tag recommendations, each formed by appending a number to the original tag.
+    """
     return [f"{tag}-{i}" for i in range(1, 4)]
 
 
 def find_tag_in_string(s):
-    """Finds and returns tags within a string that start with '%'."""
+    """
+    Finds and returns tags within a string that start with '%'.
+
+    Args:
+        s (str): The input string to search for tags.
+
+    Returns:
+        list: A list of tags found in the input string.
+
+    """
     tags = re.findall(r"%(\w+)", s)
     return tags
 
 
 def setup(bot):
-    """Adds the TagCommands cog to the bot."""
+    """
+    Adds the TagCommands cog to the bot.
+
+    Parameters:
+    - bot: The bot instance to add the cog to.
+    """
     bot.add_cog(TagCommands(bot))
 
+
 def build_embed(tag_info, username):
-    """Builds an embed for a tagged message."""
-    created_at = datetime.datetime.strptime(
-        tag_info["created_at"], "%Y-%m-%d %H:%M:%S"
-    )
+    """
+    Builds an embed for a tagged message.
+
+    Args:
+        tag_info (dict): A dictionary containing information about the tag.
+            It should have the following keys:
+            - 'tag': The tag name.
+
+            - 'content':
+            The content of the tag.
+
+            - 'created_at':
+            The creation date and time of the tag in the format '%Y-%m-%d %H:%M:%S'.
+
+            - 'usage_count': The number of times the tag has been used.
+
+        username (str): The username of the user who created the tag.
+
+    Returns:
+        disnake.Embed: An embed object representing the tagged message.
+
+    """
+    created_at = datetime.datetime.strptime(tag_info["created_at"], "%Y-%m-%d %H:%M:%S")
     created_at_formatted = created_at.strftime("%d/%m/%Y at %H:%M")
 
     embed = disnake.Embed(title=f"Tag: {tag_info['tag']}", color=disnake.Color.blue())
     embed.add_field(name="Content", value=tag_info["content"], inline=False)
     embed.add_field(name="Created by", value=username, inline=True)
-    embed.add_field(name="added", value=created_at_formatted, inline=True)
+    embed.add_field(name="Added", value=created_at_formatted, inline=True)
     embed.add_field(
         name="Number of calls",
         value=str(tag_info["usage_count"]),
--- db.py
+++ db.py
@@ -1,3 +1,38 @@
+"""
+This module provides functions for interacting with a SQLite
+database using the aiosqlite library.
+It includes functions for setting up the database, adding, retrieving,
+deleting, updating, and resetting messages.
+
+Functions:
+- db_setup():
+Sets up the database by creating the necessary tables.
+
+- add_message(server_id, tag, content, created_by):
+Adds a new message to the database.
+
+- get_similar_tags(server_id, tag):
+Retrieves tags similar to the given one from the database.
+
+- get_message(server_id, tag):
+Retrieves a specific message by tag from the database.
+
+- delete_message(server_id, tag):
+Deletes a message associated with a tag from the database.
+
+- update_message(server_id, tag, content):
+Updates the content of a message associated with a tag in the database.
+
+- get_all_messages(server_id):
+Retrieve all messages and their details from the database for a specific server.
+
+- increment_usage_count(server_id, tag):
+Increments the usage count for a specific tag.
+
+- reset_usage_count(server_id, tag):
+Resets the usage count for a specific tag to zero.
+"""
+
 import aiosqlite
 
 DB_PATH = "database.db"
@@ -45,7 +80,11 @@ async def get_message(server_id, tag):
     """Retrieves a specific message by tag from the database."""
     async with aiosqlite.connect(DB_PATH) as db:
         cursor = await db.execute(
-            "SELECT tag, content, created_by, created_at, usage_count FROM messages WHERE server_id = ? AND tag = ?",
+            """
+            SELECT
+                tag, content, created_by, created_at, usage_count
+            FROM messages
+            WHERE server_id = ? AND tag = ?""",
             (server_id, tag),
         )
         row = await cursor.fetchone()
