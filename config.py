d8f61254b0b3f3e400b908acd052aa570f7cffb4
Clara Cassinat <clara.cassinat@gmail.com>
2024-04-03 01:21:20 +0200

    refactor: uses .env if exists

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 """
 This module provides a function to load configuration
-variables from environment variables and a .env file.
+variables from environment variables. It also attempts
+to load these from a .env file if it exists.
 """
 
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = ".env"
+
+if os.path.exists(env_path):
+    load_dotenv(env_path)
+
 TOKEN = os.getenv("DISCORD_TOKEN")
